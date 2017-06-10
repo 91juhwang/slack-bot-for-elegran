225fa8a51f39146ccdac476a33bb96d38b42344b
91juhwang <91juhwang@gmail.com>
2017-06-10 15:56:33 -0400

    Install body-parser and add heroku config

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express')
+var bodyParser = require('body-parser');
+var app = express()
+var port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+  res.send('Hello World')
+})
+
+app.listen(port, function(){
+	console.log('listening on port ' + port);
+});
+
+app.post('/friday!!', function(req, res, next) {
+	var userName = req.body.user_name;
+	var botPayload = {
+		text: "Did someone say Friday?? "
+	};
+
+	if (userName !== 'slackbot') {
+		return res.status(200).json(botPayload);
+	} else {
+		return res.status(200).end();
+	}
+});
+
+app.post('/motivate me', function(req, res, next) {
+	var userName = req.body.user_name;
+	var botPayload = {
+		text: "Hey " + username + ", keep being awesome.  - Elegran bot "
+	};
+
+	if (userName !== 'slackbot') {
+		return res.status(200).json(botPayload);
+	} else {
+		return res.status(200).end();
+	}
+});
